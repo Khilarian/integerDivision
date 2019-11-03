97f59e37543ea4324e6aabea57a2ebd26801003c
Kirill Rumakin <donnorvator@gmail.com>
2019-11-03 12:51:02 +0300

    First attempt without tests

---
+++ src/main/java/com/foxminded/integerDivision/AppRunner.java
@@ -0,0 +1,10 @@
+package com.foxminded.integerDivision;
+
+public class AppRunner {
+   
+    
+    public static void main(String [] args) {
+        IntegerDivision iD = new IntegerDivision();
+        iD.drawDivision(1000,10);
+    }
+}
